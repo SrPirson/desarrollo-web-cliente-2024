104e52f9eddbfa6456eec8f0105bcdbd32016cd1
SrPirson <169342716+SrPirson@users.noreply.github.com>
2024-10-14 19:52:06 +0200

    Ejercicios casa 14/10/2024

--- relacion-01/ejercicio05_Arrays/JS/app.js
+++ relacion-01/ejercicio05_Arrays/JS/app.js
@@ -6,17 +6,16 @@ let resultadoElement = document.getElementById("resultado");
 // Mostrar la primera y última palabra en el documento usando innerHTML
 resultadoElement.innerHTML = 
     "Total de palabras: " + fraseOriginal.length + "<br>" +
-    "Primera palabra: " + fraseOriginal[0] + "<br>" +
-    "Última palabra: " + fraseOriginal[fraseOriginal.length - 1] + "<br>" +
+    "Primera palabra: " + fraseOriginal.indexOf() + "<br>" +
+    "Última palabra: " + fraseOriginal.lastIndexOf() + "<br>" +
     "Frase a la inversa: " + fraseOriginal.reverse() + "<br>" +
     "Frase ordenada alfabeticamente: " + fraseOriginal.sort() + "<br>" +
     "Frase ordenada a la inversa: " + fraseOriginal.sort().reverse();
-                             
 
 // También mostrar en la consola
 console.log("Total de palabras: " + fraseOriginal.length);
-console.log("Primera palabra: " + fraseOriginal[0]);
-console.log("Última palabra: " + fraseOriginal[fraseOriginal.length - 1]);
+console.log("Primera palabra: " + fraseOriginal.indexOf());
+console.log("Última palabra: " + fraseOriginal.lastIndexOf());
 console.log("Frase a la inversa: " + fraseOriginal.reverse());
 console.log("Frase ordenada alfabeticamente: " + fraseOriginal.sort());
 console.log("Frase ordenada a la inversa: " + fraseOriginal.sort().reverse());
